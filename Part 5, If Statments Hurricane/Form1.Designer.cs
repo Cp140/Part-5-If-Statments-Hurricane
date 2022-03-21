109ceb769df9ce8e967d0aa37978293dad4f8f38
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-03-21 11:01:21 -0400

    Add project files.

---
+++ Part 5, If Statments Hurricane/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Part_5__If_Statments_Hurricane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtWindSpeed = new System.Windows.Forms.TextBox();
+            this.lblWindSpeedMPH = new System.Windows.Forms.Label();
+            this.lblHuricaneCat = new System.Windows.Forms.Label();
+            this.lblHurricaneCatOutput = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblWindSpeedKNOTS = new System.Windows.Forms.Label();
+            this.lblWindSpeedKPH = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(60, 7);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(148, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Hurricane Status";
+            // 
+            // txtWindSpeed
+            // 
+            this.txtWindSpeed.Location = new System.Drawing.Point(163, 34);
+            this.txtWindSpeed.Name = "txtWindSpeed";
+            this.txtWindSpeed.Size = new System.Drawing.Size(100, 20);
+            this.txtWindSpeed.TabIndex = 1;
+            // 
+            // lblWindSpeedMPH
+            // 
+            this.lblWindSpeedMPH.AutoSize = true;
+            this.lblWindSpeedMPH.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWindSpeedMPH.Location = new System.Drawing.Point(12, 33);
+            this.lblWindSpeedMPH.Name = "lblWindSpeedMPH";
+            this.lblWindSpeedMPH.Size = new System.Drawing.Size(124, 18);
+            this.lblWindSpeedMPH.TabIndex = 2;
+            this.lblWindSpeedMPH.Text = "Wind speed MPH";
+            // 
+            // lblHuricaneCat
+            // 
+            this.lblHuricaneCat.AutoSize = true;
+            this.lblHuricaneCat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHuricaneCat.Location = new System.Drawing.Point(12, 116);
+            this.lblHuricaneCat.Name = "lblHuricaneCat";
+            this.lblHuricaneCat.Size = new System.Drawing.Size(131, 18);
+            this.lblHuricaneCat.TabIndex = 3;
+            this.lblHuricaneCat.Text = "Huricane Category";
+            // 
+            // lblHurricaneCatOutput
+            // 
+            this.lblHurricaneCatOutput.AutoSize = true;
+            this.lblHurricaneCatOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHurricaneCatOutput.Location = new System.Drawing.Point(160, 116);
+            this.lblHurricaneCatOutput.Name = "lblHurricaneCatOutput";
+            this.lblHurricaneCatOutput.Size = new System.Drawing.Size(31, 18);
+            this.lblHurricaneCatOutput.TabIndex = 4;
+            this.lblHurricaneCatOutput.Text = "Cat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "KNOTS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "KPH";
+            // 
+            // lblWindSpeedKNOTS
+            // 
+            this.lblWindSpeedKNOTS.AutoSize = true;
+            this.lblWindSpeedKNOTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWindSpeedKNOTS.Location = new System.Drawing.Point(12, 87);
+            this.lblWindSpeedKNOTS.Name = "lblWindSpeedKNOTS";
+            this.lblWindSpeedKNOTS.Size = new System.Drawing.Size(142, 18);
+            this.lblWindSpeedKNOTS.TabIndex = 8;
+            this.lblWindSpeedKNOTS.Text = "Wind speed KNOTS";
+            // 
+            // lblWindSpeedKPH
+            // 
+            this.lblWindSpeedKPH.AutoSize = true;
+            this.lblWindSpeedKPH.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWindSpeedKPH.Location = new System.Drawing.Point(12, 60);
+            this.lblWindSpeedKPH.Name = "lblWindSpeedKPH";
+            this.lblWindSpeedKPH.Size = new System.Drawing.Size(121, 18);
+            this.lblWindSpeedKPH.TabIndex = 9;
+            this.lblWindSpeedKPH.Text = "Wind speed KPH";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWindSpeedKPH);
+            this.Controls.Add(this.lblWindSpeedKNOTS);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHurricaneCatOutput);
+            this.Controls.Add(this.lblHuricaneCat);
+            this.Controls.Add(this.lblWindSpeedMPH);
+            this.Controls.Add(this.txtWindSpeed);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtWindSpeed;
+        private System.Windows.Forms.Label lblWindSpeedMPH;
+        private System.Windows.Forms.Label lblHuricaneCat;
+        private System.Windows.Forms.Label lblHurricaneCatOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblWindSpeedKNOTS;
+        private System.Windows.Forms.Label lblWindSpeedKPH;
+    }
+}
+
